fd188935d7acb3829d62e9b311f8cde5ffbd45cb
kasshii28 <gp22a043@oecu.jp>
2025-03-30 23:53:19 +0900

    add:コースごとの詳細を作成

---
+++ src/app/constants/courseDescriptions/courseDescriptions.ts
@@ -0,0 +1,64 @@
+export const CourseDescriptions = [
+  {
+    'C': {
+      title: 'C言語',
+      content: 'C言語のタイピングコースです',
+      url: '/routes/C'
+    },
+    'C#': {
+      title: 'C#',
+      content: 'C#のタイピングコースです',
+      url: '/routes/C#'
+    },
+    'C++': {
+      title: 'C++',
+      content: 'C++のタイピングコースです',
+      url: '/routes/C++'
+    },
+    'Go': {
+      title: 'Go',
+      content: 'Goのタイピングコースです',
+      url: '/routes/Go'
+    },
+    'Java': {
+      title: 'Java',
+      content: 'Javaのタイピングコースです',
+      url: '/routes/Java'
+    },
+    'Javascript': {
+      title: 'Javascript',
+      content: 'Javascriptのタイピングコースです',
+      url: '/routes/Javascript'
+    },
+    'PHP': {
+      title: 'PHP',
+      content: 'PHPのタイピングコースです',
+      url: '/routes/PHP'
+    },
+    'Python': {
+      title: 'Python',
+      content: 'Pythonのタイピングコースです',
+      url: '/routes/Python'
+    },
+    'React': {
+      title: 'React',
+      content: 'Reactのタイピングコースです',
+      url: '/routes/React'
+    },
+    'Ruby': {
+      title: 'Ruby',
+      content: 'Rubyのタイピングコースです',
+      url: '/routes/Ruby'
+    },
+    'Rust': {
+      title: 'Rust',
+      content: 'Rustのタイピングコースです',
+      url: '/routes/Rust'
+    },
+    'Typescript': {
+      title: 'Typescript',
+      content: 'Typescriptのタイピングコースです',
+      url: '/routes/Typescript'
+    }
+  }
+]
