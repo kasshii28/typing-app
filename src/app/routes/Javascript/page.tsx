855a108099b1cf59f0e4d0ecadf60cde8e6e6731
kasshii28 <gp22a043@oecu.jp>
2025-04-03 23:51:22 +0900

    fix:コンポーネントの作成に伴う修正

--- src/app/routes/Javascript/page.tsx
+++ src/app/routes/Javascript/page.tsx
@@ -1,5 +1,42 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { JavaScriptQuestions as Questions } from '@/app/constants/langs/Javascript/questions'
+import { handleKeyDown } from '@/app/handlers/handleKeyDown/handleKeyDown'
+import { QuestionDisplay } from '@/app/components/QuestionDisplay/QuestionDisplay'
+
 export default function Javascript(){
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0)
+  const [currentPostion, setCurrentPosition] = useState<number>(0)
+  const [isFinished, setIsFinished] = useState<boolean>(false)
+
+  useEffect(() => {
+    const KeyDownHandler = (e: KeyboardEvent) => {
+      handleKeyDown({
+        e,
+        Questions,
+        currentQuestionIndex,
+        setCurrentQuestionIndex,
+        currentPostion,
+        setCurrentPosition,
+        setIsFinished
+      })
+    }
+    window.addEventListener('keydown', KeyDownHandler)
+    return () => window.removeEventListener('keydown', KeyDownHandler)
+  }, [currentPostion, currentQuestionIndex, Questions], )
+
+  if (isFinished) {
+    return (
+      <div className='text-center text-2xl text-white'>
+        You have finished all the questions!
+      </div>
+    )
+  }
   return (
-    <div>JS</div>
+    <QuestionDisplay
+      question={Questions[currentQuestionIndex]}
+      currentPosition={currentPostion}
+    />
   )
 }
--- src/app/types/components/navCourse.ts
+++
@@ -1,5 +0,0 @@
-export interface NavCourseProps {
-  title: string;
-  content: string;
-  url: string;
-}
