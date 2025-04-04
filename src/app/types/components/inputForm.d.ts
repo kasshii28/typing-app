527cff47d55c0edbdcc8607f11227db17e8a0653
kasshii28 <gp22a043@oecu.jp>
2025-04-04 12:47:37 +0900

    add:入力フォームコンポーネントの作成

---
+++ src/app/types/components/inputForm.d.ts
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from "react";
+import { FieldValues, UseControllerProps } from "react-hook-form";
+
+/**
+ * 入力フォームコンポーネントのプロパティ型定義
+ *
+ * @template T - フォームのフィールド値の型
+ * @extends {Omit<InputHTMLAttributes<HTMLInputElement>, 'name'>} - name以外のHTMLInput属性
+ * @extends {UseControllerProps<T>} - React Hook Formのコントローラープロパティ
+ */
+export interface InputFormProps<T extends FieldValues>
+    extends Omit<InputHTMLAttributes<HTMLInputElement>, 'name'>,
+    UseControllerProps<T> {
+        /** 入力フィールドのラベル */
+        label: string
+    }   
